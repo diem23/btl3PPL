20b85e1bbb99761258b9056c342228893589ec19
unknown <diem.nguyenhv23@hcmut.edu.vn>
2024-04-08 00:05:33 +0700

    implementation all func (without debug)

--- src/main/zcode/checker/StaticCheck.py
+++ src/main/zcode/checker/StaticCheck.py
@@ -3,18 +3,29 @@ from Visitor import *
 from Utils import Utils
 from StaticError import *
 from functools import reduce
+NumOfLoop = 0
 class NoneType: pass
+class UseInLoop:
+    def __init__(self,name):
+        self.name = name
+class FunctionType:
+    def __init__(self,param,haveBody,returnType):
+        self.param = param
+        self.haveBody = haveBody
+        self.returnType = returnType
 class Symbol:
-    def __init__(self, name, mtype, isFunc = False, params = []):
+    def __init__(self, name, mtype):
         self.name = name
         self.mtype = mtype
-        self.isFunc = isFunc
-        self.params = params
+
 class HelpingTools:
-    def checkRedeclared(name,o):
+    def checkRedeclared(name,isFunc,o):
         for item in o[0]:
-            if item.name == name:
-                return True
+            if item.name == name :
+                if isFunc and type(item.mtype) is FunctionType:
+                    return True
+                if not isFunc and type(item.mtype) is not FunctionType:
+                    return True
         return False
     def inferType(name,Type,o):
         for scope in o:
@@ -26,6 +37,8 @@ class HelpingTools:
                         #print('infer arrayType')
                         item.mtype.eleType = Type.eleType
                         item.mtype.size += Type.size
+                    elif type(item.mtype) is FunctionType:
+                        item.mtype.returnType = Type
                     else: 
                         #print('infer type: ',Type)
                         item.mtype = Type
@@ -70,38 +83,12 @@ class HelpingTools:
                 result+=[float(name1.size[i])]
             if len(result) == 0: return name1.eleType
             return ArrayType(result,name1.eleType)
-        
-            
-        
     def checkPriTypeEquivalent(type1,type2):  ## check 2 kieu du lieu co tuong duong nhau hay khong (gom primitive type va ArrayType, khong bao gom noneType)
         if type(type1) != type(type2): return False
         if type(type1) == ArrayType:
             if type1.size != type2.size or not HelpingTools.checkPriTypeEquivalent(type1.eleType ,type2.eleType):
                 return False
         return True
-    # def getArrayType(item,o):
-    #     dimension=[]
-    #     while type(item) is list:
-    #         flag=HelpingTools.getArrayType(item[0],o)
-    #         if  not flag: return False
-    #         for i in range(1,len(item)):
-    #             element = HelpingTools.getArrayType(item[i],o)
-    #             if not element: return False
-    #             if type(element.eleType) is NoneType:
-    #                 if type(flag.eleType) is not NoneType: 
-    #                     HelpingTools.inferType(item[i].name,flag.eleType,o)
-    #             elif type(flag.eleType) is NoneType:
-    #                 i = 1
-    #                 HelpingTools.inferType(item[0].name,element.eleType,o)
-    #                 flag.eleType = element.eleType
-    #                 flag.size = element.size
-    #             elif element.size != flag.size or not HelpingTools.checkPriTypeEquivalent(element.eleType,flag.eleType):
-    #                 return False
-    #         dimension.append(float(len(item)))
-    #         item=item[0]
-    #     print(type(item))
-    #     print (type(item) is Id, type(item) is ArrayLiteral)
-    #     return ArrayType(dimension,StaticChecker(item).visit(item,o))
     
     def getArrayType(item,dimension,names,o,canUpdate):  ## dimension la mang chua kich thuoc cua cac chieu cua mang, names gom cac NoneType Id, canUpdate cho phep cap nhat kich thuoc cua mang hay khong
         if type(item) is ArrayLiteral:
@@ -159,7 +146,7 @@ class HelpingTools:
                 #print(typEle)
                 #print(names)
                 #print(type(firstEleTyp),type(typEle))
-                if type(firstEleTyp) is NoneType and type(typEle) is not NoneType: ## neu kieu du lieu cua phan tu dau tien la NoneType thì tất cả các phần tử đầu tiên đến trước phần tử hiện tại đều là NoneType
+                if type(firstEleTyp) is NoneType  and type(typEle) is not NoneType: ## neu kieu du lieu cua phan tu dau tien la NoneType thì tất cả các phần tử đầu tiên đến trước phần tử hiện tại đều là NoneType
                     #print('go in here!!!')
                     #print('names: ',names)
                     for i in names:         ## names là list chứa các biến có kiểu dữ liệu là NoneType hoặc ArrayType có eleType là NoneType tư phần tử đầu tiên đến trước phần tử hiện tại
@@ -218,7 +205,18 @@ class HelpingTools:
             if type(getType) is NoneType :  ## gap Id voi kieu du lieu chua xac dinh
                 names+=[item.name]
             return getType
-                
+    def customVisitId(name,o,isFunction=False):
+        for scope in o:
+            for i in scope:
+                if i.name == name:
+                    if isFunction and type(i.mtype) is FunctionType:
+                        return i.mtype
+                    if not isFunction and type(i.mtype) is not FunctionType:
+                        return i.mtype
+        if isFunction:
+            raise Undeclared(Function(),name)
+        else: raise Undeclared(Identifier(),name)
+                    
         
     def printO(o):
         for scope in o:
@@ -237,24 +235,24 @@ class StaticChecker(BaseVisitor, Utils):
         #print(self.ast)
         o=[[]]
         ## declare build-in functions
-        o[0]+=[Symbol('readNumber',NumberType(),True)]
-        o[0]+=[Symbol('writeNumber',VoidType(),True,[NumberType()])]
-        o[0]+=[Symbol('readBool',BoolType(),True,)]
-        o[0]+=[Symbol('writeBool',VoidType(),True,[BoolType()])]
-        o[0]+=[Symbol('readString',StringType(),True,)]
-        o[0]+=[Symbol('writeString',VoidType(),True,[StringType()])]
+        o[0]+=[Symbol('readNumber',FunctionType([],True,NumberType()))]
+        o[0]+=[Symbol('writeNumber',FunctionType([NumberType()],True,VoidType()))]
+        o[0]+=[Symbol('readBool',FunctionType([],True,BoolType()))]
+        o[0]+=[Symbol('writeBool',FunctionType([BoolType()],True,VoidType()))]
+        o[0]+=[Symbol('readString',FunctionType([],True,StringType()))]
+        o[0]+=[Symbol('writeString',FunctionType([StringType()],True,VoidType()))]
         for i in ctx.decl:
             if isinstance(i,VarDecl):
-                if HelpingTools.checkRedeclared(i.name.name,o):
+                if HelpingTools.checkRedeclared(i.name.name,False,o):
                     raise Redeclared(i.name.name,"Variable")
             elif isinstance(i,FuncDecl):
-                if HelpingTools.checkRedeclared(i.name.name,o):
+                if HelpingTools.checkRedeclared(i.name.name,True,o):
                     raise Redeclared(i.name.name,"Function")
             self.visit(i, o)
         flagEntry= False
-        HelpingTools.printO(o)
+        #HelpingTools.printO(o)
         for item in o[len(o)-1]:
-            if item.isFunc and item.name == 'main':
+            if type(item.mtype) is FunctionType and item.name == 'main':
                 flagEntry = True
                 break
         if not flagEntry:
@@ -268,10 +266,12 @@ class StaticChecker(BaseVisitor, Utils):
                 rhs=self.visit(ctx.varInit,o)
             if type(rhs) is NoneType and type(ctx.varType) == type(None):   ## both are NoneType
                 raise TypeCannotBeInferred(ctx.name.name)
-            elif type(rhs) is NoneType and type(ctx.varType) == type(None): ## rhs is NoneType
-                HelpingTools.inferType(ctx.varInit.name,ctx.value,o)
+            elif type(rhs) is NoneType and type(ctx.varType) != type(None): ## rhs is NoneType
+                HelpingTools.inferType(ctx.varInit.name,ctx.varType,o)
                 o[0]+=[Symbol(ctx.name.name,ctx.varType)]
             elif type(rhs) is not NoneType and type(ctx.varType) == type(None): ## ctx.varType is NoneType
+                if type(rhs) is ArrayType and type(rhs.eleType) is NoneType:
+                    raise TypeCannotBeInferred(ctx)
                 o[0]+=[Symbol(ctx.name.name,rhs)]
             else:                                                               ## both are not NoneType
                 if type(rhs) != type(ctx.varType):                              ## type mismatch
@@ -293,11 +293,112 @@ class StaticChecker(BaseVisitor, Utils):
             if type(ctx.varType) == type(None):
                 o[0]+= [Symbol(ctx.name.name,NoneType())]
             else: o[0]+=[Symbol(ctx.name.name,ctx.varType)]
+    def visitFuncDecl(self,ctx:FuncDecl,o:object):
+        param_list=[]
+        env = [[]] + o
+        param_list=[self.visit(i,env) for i in ctx.param]
+        newFunc=[Symbol(ctx.name.name,FunctionType(param_list,NoneType()))]
+        o[0]+=newFunc
+        if type(ctx.body) is not type(None):  ## nếu func có body
+            env[1]+=newFunc                     ## thêm tên func vào scope trước đó của env
+            returnType=self.visit(ctx.body,env)     ## kiểm tra kiểu trả về của func
+            if type(returnType) is UseInLoop:
+                raise MustInLoop(returnType.name)
+            newFunc[0].mtype.returnType=returnType  ## cập nhật kiểu trả về của func
+            newFunc[0].mtype.haveBody=True
+            o[0]=o[0][:-1] + newFunc                ## xóa func có kiểu trả về NoneType ra khỏi o và thêm func có kiểu trả về đã cập nhật vào o
+
+    def visitAssign(self,ctx,o:object):
+        lhs = self.visit(ctx.lhs)
+        rhs = self.visit(ctx.rhs)
+        if type(lhs) is NoneType and type(rhs) is NoneType:
+            raise TypeCannotBeInferred(ctx)   
+        elif type(lhs) is NoneType and type(rhs) is not NoneType:
+            HelpingTools.inferType(lhs.name,rhs,o)
+        elif type(lhs) is not NoneType and type(rhs) is  NoneType:
+            HelpingTools.inferType(rhs.name,lhs,o)
+        elif not HelpingTools.checkPriTypeEquivalent(rhs,lhs):
+            raise TypeMismatchInStatement(ctx)
+        return VoidType()
+    def visitIf(self,ctx,o:object):
+        returnType=VoidType()
+        ifExpr=self.visit(ctx.expr)
+        if type(ifExpr) is NoneType:
+            HelpingTools.inferType(ifExpr.name,BoolType(),o)
+        elif type(ifExpr) is not BoolType:
+            raise TypeMismatchInStatement(ctx)
+        for i in ctx.elifStmt:
+            expr=self.visit(i[0],o)
+            if type(expr) is NoneType:
+                HelpingTools.inferType(expr.name,BoolType(),o)
+            elif type(expr) is not BoolType:
+                raise TypeMismatchInStatement(ctx)
+            stmtType=self.visit(i[1],o)
+            if type(stmtType) is not VoidType and type(returnType) is VoidType :
+                returnType=stmtType    
+        return returnType
+    def visitFor(self,ctx,o:object): 
+        Id=self.visit(ctx.name,o)
+        if type(Id) is NoneType:
+            HelpingTools.inferType(ctx.name.name,NumberType(),o)
+            Id=NumberType()   
+        elif type(Id) is not NumberType:
+            raise TypeMismatchInStatement(ctx)
+        conExpr=self.visit(ctx.condExpr,o)
+        if type(conExpr) is NoneType:
+            HelpingTools.inferType(ctx.condExpr.name,BoolType(),o)
+        elif type(conExpr) is not BoolType:
+            raise TypeMismatchInStatement(ctx)
+        updExpr=self.visit(ctx.updExpr,o)
+        if type(updExpr) is NoneType:
+            HelpingTools.inferType(ctx.updExpr.name,NumberType(),o)
+        elif type(updExpr) is not NumberType:
+            raise TypeMismatchInStatement(ctx)
+        NumOfLoop+=1
+        stmt=self.visit(ctx.body,o)
+        NumOfLoop-=1
+        return stmt
+    def visitBreak(self,ctx,o:object): 
+        if NumOfLoop==0:
+            raise MustInLoop('break')
+        return VoidType()
+    def visitContinue(self,ctx,o:object):
+        if NumOfLoop==0:
+            raise MustInLoop('continue')
+        return VoidType()
+    def visitReturn(self,ctx,o:object):
+        if type(ctx.expr) is not type(None):
+            returnType=self.visit(ctx.expr,o)
+            if type(returnType) is NoneType:
+                raise TypeCannotBeInferred(ctx)
+            return returnType
+        return VoidType()  
+    def visitCallStmt(self,ctx,o:object):
+        Id = HelpingTools.customVisitId(ctx.name,o,True)
+        if Id.returnType is not VoidType:
+            raise TypeMismatchInStatement(ctx)
+        if len(ctx.args) != len(Id.param):
+            raise TypeMismatchInStatement(ctx)
+        for i in range(0,len(ctx.args)):
+            arg = self.visit(ctx.args[i],o)
+            if type(arg) is NoneType:
+                HelpingTools.inferType(ctx.args[i].name,Id.param[i],o)
+            elif not HelpingTools.checkPriTypeEquivalent(arg,Id.param[i]):
+                raise TypeMismatchInStatement(ctx)
+        return VoidType()
+    def visitBlock(self,ctx,o:object):
+        returnType=VoidType()
+        newScope=[[]]+o
+        for i in ctx.member:
+            stmt=self.visit(i,newScope)
+            if type(stmt) is not VoidType and type(returnType) is VoidType:
+                returnType=stmt
+        return returnType
     def BinOp(self,ctx,o):
         #print(ctx)
         left = self.visit(ctx.left,o)
         right = self.visit(ctx.right,o)
-        if ctx.op in ['+','-','*','/','%']:
+        if ctx.op in ['+','-','*','/','%']:   # operantype is numbertype, return type is numbertype
             if type(right) is not NoneType and type(right) is not NoneType:
                 if not (type(right) is NumberType and type(left) is NumberType):
                     raise TypeMismatchInExpression(ctx)
@@ -308,7 +409,7 @@ class StaticChecker(BaseVisitor, Utils):
                 HelpingTools.inferType(ctx.right.name,NumberType(),o)
                 right = NumberType()
             return NumberType()
-        elif ctx.op in ['and','or']:
+        elif ctx.op in ['and','or']: ## operantype is booltype, return type is booltype
             if type(right) is not NoneType and type(right) is not NoneType:
                 if not (type(right) is BoolType and type(left) is BoolType):
                     raise TypeMismatchInExpression(ctx)
@@ -319,7 +420,7 @@ class StaticChecker(BaseVisitor, Utils):
                 HelpingTools.inferType(ctx.right.name,BoolType(),o)
                 right = BoolType()
             return BoolType()
-        elif ctx.op in ['...']:
+        elif ctx.op in ['...']: ## operantype is stringtype, return type is stringtype
             if type(right) is not NoneType and type(right) is not NoneType:
                 if not (type(right) is StringType and type(left) is StringType):
                     raise TypeMismatchInExpression(ctx)
@@ -330,7 +431,7 @@ class StaticChecker(BaseVisitor, Utils):
                 HelpingTools.inferType(ctx.right.name,StringType(),o)
                 right = StringType()
             return StringType()
-        elif ctx.op in['<','<=','>','>=','!=','=']:
+        elif ctx.op in['<','<=','>','>=','!=','=']: ## operantype is numbertype, return type is booltype
             if type(right) is not NoneType and type(right) is not NoneType:
                 if not (type(right) is NumberType and type(left) is NumberType):
                     raise TypeMismatchInExpression(ctx)
@@ -341,7 +442,7 @@ class StaticChecker(BaseVisitor, Utils):
                 HelpingTools.inferType(ctx.right.name,NumberType(),o)
                 right = NumberType()
             return BoolType()
-        elif ctx.op in['==']:
+        elif ctx.op in['==']:   ## operantype is numbertype or stringtype, return type is booltype
             if type(right) is not NoneType and type(right) is not NoneType:
                 if not (type(right) is StringType and type(left) is StringType):
                     raise TypeMismatchInExpression(ctx)
@@ -353,15 +454,60 @@ class StaticChecker(BaseVisitor, Utils):
                 right = StringType()
             return BoolType()
         raise TypeMismatchInExpression(ctx)
+    def Unop(self,ctx,o):
+        operand = self.visit(ctx.operand,o)
+        if ctx.op in ['-','+']:
+            if type(operand) is not NoneType:
+                if not type(operand) is NumberType:
+                    raise TypeMismatchInExpression(ctx)
+            else:
+                HelpingTools.inferType(ctx.operand.name,NumberType(),o)
+                operand = NumberType()
+            return NumberType()
+        elif ctx.op in ['not']:
+            if type(operand) is not NoneType:
+                if not type(operand) is BoolType:
+                    raise TypeMismatchInExpression(ctx)
+            else:
+                HelpingTools.inferType(ctx.operand.name,BoolType(),o)
+                operand = BoolType()
+            return BoolType()
+
     def visitId(self,ctx:Id,o:object):
-        #print('visitId')
-        for scope in o:
-            for item in scope:
-                if item.name == ctx.name:
-                    #print(item.name,item.mtype)
-                    return item.mtype
-        
-        raise Undeclared(Identifier(),ctx.name)    
+        returnType= HelpingTools.customVisitId(ctx.name,o)
+        return returnType   
+    def visitArrayCell(self,ctx:ArrayCell,o:object):
+        arr=self.visit(ctx.arr,o)
+        if type(arr) is NoneType:       ## if arr is NoneType, can not infer type of arr
+            raise TypeCannotBeInferred(ctx)  
+        elif type(arr) is not ArrayType: ## if arr is not ArrayType, raise error
+            raise TypeMismatchInExpression(ctx)
+        for i in ctx.idx:
+            idx=self.visit(i,o)
+            if type(idx) is NoneType:
+                HelpingTools.inferType(i.name,NumberType(),o)
+            elif type(idx) is not NumberType:
+                raise TypeMismatchInExpression(ctx)
+        if len(ctx.idx) > len(arr.size):   ## invalid subscript
+            raise TypeMismatchInExpression(ctx)
+        elif len(ctx.idx) < len(arr.size):      ## return type of array cell is ArrayType
+            return ArrayType(arr.size[len(ctx.idx):],arr.eleType)
+        return arr.eleType
+    def visitCallExpr(self,ctx:CallExpr,o:object):
+        Id = helpingTools.customVisitId(ctx.name,o,True)
+        if type(Id) is not FunctionType:
+            raise TypeMismatchInExpression(ctx)
+        if type(Id.returnType) is VoidType:
+            raise TypeMismatchInExpression(ctx)
+        if len(ctx.args) != len(Id.param):
+            raise TypeMismatchInExpression(ctx)
+        for i in range(0,len(ctx.args)):
+            arg = self.visit(ctx.args[i],o)
+            if type(arg) is NoneType:
+                HelpingTools.inferType(ctx.args[i].name,Id.param[i],o)
+            elif not HelpingTools.checkPriTypeEquivalent(arg,Id.param[i]):
+                raise TypeMismatchInExpression(ctx)
+        return Id.returnType
     def visitNumberLiteral(self,ctx:NumberLiteral,o:object):
         return NumberType()
     def visitBooleanLiteral(self,ctx:BooleanLiteral,o:object):
--- src/test/CheckerSuite.py
+++ src/test/CheckerSuite.py
@@ -7,97 +7,97 @@ from main.zcode.utils.AST import *
 
 class CheckerSuite(unittest.TestCase):
     #invalid_arrayliteral
-    # def test_001(self):
-    #     input = """var a <- [[1,2],[1,2,3]]
-    #     """
-    #     expect = "Type Mismatch In Expression: ArrayLit(ArrayLit(NumLit(1.0), NumLit(2.0)), ArrayLit(NumLit(1.0), NumLit(2.0), NumLit(3.0)))"
-    #     self.assertTrue(TestChecker.test(input, expect, 401))
-    # def test_002(self):
-    #     input = """
-    #     string b
-    #     var a <- [[1,2,4],[1,2,3],[b,b,b]]
-    #     """
-    #     expect = "Type Mismatch In Expression: ArrayLit(ArrayLit(NumLit(1.0), NumLit(2.0), NumLit(4.0)), ArrayLit(NumLit(1.0), NumLit(2.0), NumLit(3.0)), ArrayLit(Id(b), Id(b), Id(b)))"
-    #     self.assertTrue(TestChecker.test(input, expect, 402))
-    # def test_003(self):
-    #     input = """var a <- [[1,2,3],[1,2,3],[1,2,3,4],[1,2,3]]
-    #     """
-    #     expect = "Type Mismatch In Expression: ArrayLit(ArrayLit(NumLit(1.0), NumLit(2.0), NumLit(3.0)), ArrayLit(NumLit(1.0), NumLit(2.0), NumLit(3.0)), ArrayLit(NumLit(1.0), NumLit(2.0), NumLit(3.0), NumLit(4.0)), ArrayLit(NumLit(1.0), NumLit(2.0), NumLit(3.0)))"
-    #     self.assertTrue(TestChecker.test(input, expect, 403))
-    # def test_004(self):
-    #     input = """
-    #     dynamic t
-    #     var a <- [[1,2],[1,t],[1,2,3]]
-    #     """
-    #     expect = "Type Mismatch In Expression: ArrayLit(ArrayLit(NumLit(1.0), NumLit(2.0)), ArrayLit(NumLit(1.0), Id(t)), ArrayLit(NumLit(1.0), NumLit(2.0), NumLit(3.0)))"
-    #     self.assertTrue(TestChecker.test(input, expect, 404))
-    # def test_005(self):
-    #     input = """
-    #     dynamic ans
-    #     dynamic temp <- "abc"
-    #     var a <- [[1,2],[1,ans],[temp,ans]]
-    #     """
-    #     expect = "Type Mismatch In Expression: ArrayLit(ArrayLit(NumLit(1.0), NumLit(2.0)), ArrayLit(NumLit(1.0), Id(ans)), ArrayLit(Id(temp), Id(ans)))"
-    #     self.assertTrue(TestChecker.test(input, expect, 405))
-    # def test_006(self):
-    #     input = """
-    #     dynamic ans
-    #     number a[3.0,4.0] <- [[ans,ans,ans,ans],[1,2,ans,4],[1,2,3,4]]
-    #     """
-    #     expect = "No Entry Point"
-    #     self.assertTrue(TestChecker.test(input, expect, 406))
-    # def test_007(self):
-    #     input = """number a[2,3] <- [[2,3],[2,3]]
-    #     """
-    #     expect = "Type Mismatch In Statement: VarDecl(Id(a), ArrayType([2.0, 3.0], NumberType), None, ArrayLit(ArrayLit(NumLit(2.0), NumLit(3.0)), ArrayLit(NumLit(2.0), NumLit(3.0))))"
-    #     self.assertTrue(TestChecker.test(input, expect, 407))
-    # def test_008(self):
-    #     input = """
-    #     dynamic t
-    #     dynamic b
-    #     dynamic c
-    #     dynamic d
-    #     dynamic e
-    #     dynamic f
-    #     number a[2,3] <- [[b,t,d],[c,e,f]]
-    #     """
-    #     expect = "No Entry Point"
-    #     self.assertTrue(TestChecker.test(input, expect, 408))
-    # def test_009(self):
-    #     input = """
-    #     dynamic t
-    #     dynamic b
-    #     dynamic c
-    #     dynamic d
-    #     dynamic e
-    #     dynamic f
-    #     number a[2.0,3.0,2.0] <- [[[b,c],[d,d],[t,f]],[[e,e],[e,t],[f,d]]]
-    #     """
-    #     expect = "No Entry Point"
-    #     self.assertTrue(TestChecker.test(input, expect, 409))
-    # def test_010(self):
-    #     input = """
-    #     dynamic t
-    #     dynamic b
-    #     dynamic c
-    #     dynamic d
-    #     dynamic e
-    #     dynamic f
-    #     number a[2.0,3.0,2.0] <- [[[1,c],[3,d],[t,f]],[[b,9],[e,t],[2,d]]]
-    #     """
-    #     expect = "No Entry Point"
-    #     self.assertTrue(TestChecker.test(input, expect, 410))
-#     def test455(self):
-#         input = """
-#     dynamic a
-#     dynamic b
-#     dynamic c
-#     number arr[1, 2,2] <- [[a, b]]
-#     ## number c[2, 2] <- arr
-# """
-#         expect = "Type Mismatch In Statement: VarDecl(Id(arr), ArrayType([2.0, 2.0], NumberType), None, ArrayLit(ArrayLit(Id(a), Id(b))))"
-#         self.assertTrue(TestChecker.test(input, expect, 455))
-    def test485(self):
+    def test_001(self):
+        input = """var a <- [[1,2],[1,2,3]]
+        """
+        expect = "Type Mismatch In Expression: ArrayLit(ArrayLit(NumLit(1.0), NumLit(2.0)), ArrayLit(NumLit(1.0), NumLit(2.0), NumLit(3.0)))"
+        self.assertTrue(TestChecker.test(input, expect, 401))
+    def test_002(self):
+        input = """
+        string b
+        var a <- [[1,2,4],[1,2,3],[b,b,b]]
+        """
+        expect = "Type Mismatch In Expression: ArrayLit(ArrayLit(NumLit(1.0), NumLit(2.0), NumLit(4.0)), ArrayLit(NumLit(1.0), NumLit(2.0), NumLit(3.0)), ArrayLit(Id(b), Id(b), Id(b)))"
+        self.assertTrue(TestChecker.test(input, expect, 402))
+    def test_003(self):
+        input = """var a <- [[1,2,3],[1,2,3],[1,2,3,4],[1,2,3]]
+        """
+        expect = "Type Mismatch In Expression: ArrayLit(ArrayLit(NumLit(1.0), NumLit(2.0), NumLit(3.0)), ArrayLit(NumLit(1.0), NumLit(2.0), NumLit(3.0)), ArrayLit(NumLit(1.0), NumLit(2.0), NumLit(3.0), NumLit(4.0)), ArrayLit(NumLit(1.0), NumLit(2.0), NumLit(3.0)))"
+        self.assertTrue(TestChecker.test(input, expect, 403))
+    def test_004(self):
+        input = """
+        dynamic t
+        var a <- [[1,2],[1,t],[1,2,3]]
+        """
+        expect = "Type Mismatch In Expression: ArrayLit(ArrayLit(NumLit(1.0), NumLit(2.0)), ArrayLit(NumLit(1.0), Id(t)), ArrayLit(NumLit(1.0), NumLit(2.0), NumLit(3.0)))"
+        self.assertTrue(TestChecker.test(input, expect, 404))
+    def test_005(self):
+        input = """
+        dynamic ans
+        dynamic temp <- "abc"
+        var a <- [[1,2],[1,ans],[temp,ans]]
+        """
+        expect = "Type Mismatch In Expression: ArrayLit(ArrayLit(NumLit(1.0), NumLit(2.0)), ArrayLit(NumLit(1.0), Id(ans)), ArrayLit(Id(temp), Id(ans)))"
+        self.assertTrue(TestChecker.test(input, expect, 405))
+    def test_006(self):
+        input = """
+        dynamic ans
+        number a[3.0,4.0] <- [[ans,ans,ans,ans],[1,2,ans,4],[1,2,3,4]]
+        """
+        expect = "No Entry Point"
+        self.assertTrue(TestChecker.test(input, expect, 406))
+    def test_007(self):
+        input = """number a[2,3] <- [[2,3],[2,3]]
+        """
+        expect = "Type Mismatch In Statement: VarDecl(Id(a), ArrayType([2.0, 3.0], NumberType), None, ArrayLit(ArrayLit(NumLit(2.0), NumLit(3.0)), ArrayLit(NumLit(2.0), NumLit(3.0))))"
+        self.assertTrue(TestChecker.test(input, expect, 407))
+    def test_008(self):
+        input = """
+        dynamic t
+        dynamic b
+        dynamic c
+        dynamic d
+        dynamic e
+        dynamic f
+        number a[2,3] <- [[b,t,d],[c,e,f]]
+        """
+        expect = "No Entry Point"
+        self.assertTrue(TestChecker.test(input, expect, 408))
+    def test_009(self):
+        input = """
+        dynamic t
+        dynamic b
+        dynamic c
+        dynamic d
+        dynamic e
+        dynamic f
+        number a[2.0,3.0,2.0] <- [[[b,c],[d,d],[t,f]],[[e,e],[e,t],[f,d]]]
+        """
+        expect = "No Entry Point"
+        self.assertTrue(TestChecker.test(input, expect, 409))
+    def test_010(self):
+        input = """
+        dynamic t
+        dynamic b
+        dynamic c
+        dynamic d
+        dynamic e
+        dynamic f
+        number a[2.0,3.0,2.0] <- [[[1,c],[3,d],[t,f]],[[b,9],[e,t],[2,d]]]
+        """
+        expect = "No Entry Point"
+        self.assertTrue(TestChecker.test(input, expect, 410))
+    def test011(self):
+        input = """
+    dynamic a
+    dynamic b
+    dynamic c
+    number arr[1, 2,2] <- [[a, b]]
+    ## number c[2, 2] <- arr
+"""
+        expect = "No Entry Point"
+        self.assertTrue(TestChecker.test(input, expect, 411))
+    def test012(self):
         input = """
 dynamic a
     dynamic b
@@ -111,4 +111,19 @@ dynamic a
 """
         expect = "No Entry Point"
         
-        self.assertTrue(TestChecker.test(input, expect, 485))
+        self.assertTrue(TestChecker.test(input, expect, 412))
+    def test013(self):
+        input = """
+dynamic a
+    dynamic b
+    dynamic c
+    dynamic d
+    dynamic e
+    var x <- [a, [b], [[c]], [[[d, e]]] ]
+    ##c <- [-10, 2 / 3 % 0.75]
+    ##b <- [c]
+    ##a <- [b]
+"""
+        expect = "Type Cannot Be Inferred: VarDecl(Id(x), None, var, ArrayLit(Id(a), ArrayLit(Id(b)), ArrayLit(ArrayLit(Id(c))), ArrayLit(ArrayLit(ArrayLit(Id(d), Id(e))))))"
+        
+        self.assertTrue(TestChecker.test(input, expect, 413)) 
